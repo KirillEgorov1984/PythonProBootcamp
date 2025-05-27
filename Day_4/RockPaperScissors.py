6e2c9f6d768beee38ea6c171f1b3aebd40a62479
KirillEgorov1984 <49128258+KirillEgorov1984@users.noreply.github.com>
2025-05-27 14:03:57 +0300

    plus RockPaperScissors

--- Day_4/RockPaperScissors.py
+++ Day_4/RockPaperScissors.py
@@ -30,9 +30,10 @@ scissors ='''
 
 game_image = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+
 if user_choice >= 0 and user_choice <= 2:
     print(game_image[user_choice])
-1
+
 computer_choice = random.randint(0,2)
 print ("Computer chose:")
 print (game_image[computer_choice])
